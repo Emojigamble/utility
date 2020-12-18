b10517beb7da9c793ff12c5cc465b0088ac1ddb6
Adrian <hannsadrian@gmail.com>
2020-12-18 22:51:45 +0100

    :sparkles: Make Logger pretty

--- logger/go.mod
+++ logger/go.mod
@@ -1,3 +1,5 @@
 module github.com/Emojigamble/utility/logger
 
 go 1.14
+
+require github.com/logrusorgru/aurora v2.0.3+incompatible
--- logger/main.go
+++ logger/main.go
@@ -1,34 +1,71 @@
 package logger
 
 import (
-	"log"
+	"fmt"
+	"github.com/logrusorgru/aurora"
+	"strings"
 	"time"
 )
 
 type LogLevel string
 
-const(
-	GameLogLevel = "GameLogLevel"
+const (
+	GameLogLevel       = "GameLogLevel"
 	ConnectionLogLevel = "ConnectionLogLevel"
-	BaseLogLevel = "BaseLogLevel"
-	WarnLogLevel = "WarnLogLevel"
-	ErrorLogLevel = "ErrorLogLevel"
-	)
+	BaseLogLevel       = "BaseLogLevel"
+	WarnLogLevel       = "WarnLogLevel"
+	ErrorLogLevel      = "ErrorLogLevel"
+)
+
+var colors = map[LogLevel]aurora.Value{
+	GameLogLevel:       aurora.Magenta(GameLogLevel),
+	ConnectionLogLevel: aurora.Blue(ConnectionLogLevel),
+	BaseLogLevel:       aurora.Gray(15, BaseLogLevel),
+	WarnLogLevel:       aurora.Yellow(WarnLogLevel),
+	ErrorLogLevel:      aurora.Red(ErrorLogLevel),
+}
 
 type EmojigambleLogger struct {
-	ActiveLogLevels []LogLevel
-	LogToDatabase bool
+	ActiveLogLevels         []LogLevel
+	LogToDatabase           bool
 	MongoDBConnectionString string
 }
 
-func (logger *EmojigambleLogger) log(message string, level LogLevel) {
+func AllLogLevels() []LogLevel {
+	return []LogLevel{
+		GameLogLevel,
+		ConnectionLogLevel,
+		BaseLogLevel,
+		WarnLogLevel,
+		ErrorLogLevel,
+	}
+}
+
+func (logger *EmojigambleLogger) Log(message string, level LogLevel) {
+	logger.log(message, level, logger.LogToDatabase)
+}
+
+func (logger *EmojigambleLogger) log(message string, level LogLevel, logToDb bool) {
 	for _, l := range logger.ActiveLogLevels {
 		if l == level {
-			log.Println(time.Now(), level, message)
+			fmt.Println(
+				aurora.BgWhite(aurora.Gray(3, time.Now().Format(" 02.01.2006|15:04:05 "))),
+				strings.Replace(colors[level].String(), "LogLevel", "", 1), " ",
+				message)
+
+			if logToDb {
+				// TODO: save log into database
+			}
 		}
 	}
+}
 
-	if logger.LogToDatabase {
-		// TODO: save log into database
-	}
+func (logger *EmojigambleLogger) SampleLoggerOutput(logToDb bool) {
+	fmt.Println(aurora.Red("+START+ Emojigamble logger sample\n"))
+	logger.log("Game started!", GameLogLevel, logToDb)
+	logger.log("Connected!", ConnectionLogLevel, logToDb)
+	logger.log("Base log", BaseLogLevel, logToDb)
+	logger.log("Warning!", WarnLogLevel, logToDb)
+	logger.log("Error at nothing :)", ErrorLogLevel, logToDb)
+	fmt.Println(aurora.Red("\n+END+ Emojigamble logger sample"))
 }
