97a84ce2dcd5a946b9f26d2b636d1e84b8f2333a
Adrian <hannsadrian@gmail.com>
2020-12-21 13:50:34 +0100

    :lipstick: Log CORS middleware initialization

--- middleware/corsMiddleware.go
+++ middleware/corsMiddleware.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"fmt"
+	"github.com/Emojigamble/utility/logger"
 	"net/http"
 	"strings"
 )
@@ -8,10 +10,12 @@ import (
 type CorsMiddleware struct {
 	AllowedOrigins []string
 	AllowedHeaders string
+	Logger         logger.EmojigambleLogger
 }
 
 // Provides a middleware that allows Cross-Origin-Requests.
 func (cm *CorsMiddleware) Cors(next http.Handler) http.Handler {
+	cm.Logger.Log(fmt.Sprintf("Allowing the following origins: %v", cm.AllowedOrigins), logger.BaseLogLevel)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if contains(cm.AllowedOrigins, r.Header.Get("Origin")) {
 			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
--- middleware/go.mod
+++ middleware/go.mod
@@ -2,3 +2,4 @@ module github.com/Emojigamble/utility/middleware
 
 go 1.14
 
+require github.com/Emojigamble/utility/logger v0.0.0-20201221101255-3393e09df1ff
